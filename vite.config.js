f060c3dced9e1c19371571c1106de3478e14cf7e
smokydstona <smokydstona@gmail.com>
2026-03-09 15:27:34 -0700

    Fix GitHub Pages deploy for fork

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "@sqlite.org/sqlite-wasm",
     ]
   },
-  base: "/convert/",
+  base: process.env.VITE_BASE ?? "/",
   plugins: [
     viteStaticCopy({
       targets: [
